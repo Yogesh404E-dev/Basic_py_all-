8610fc5a2908e7cc3a495276836516aa4ca782d0
pythonsyp <pythonsyp@gmail.com>
2020-08-21 14:54:34 -0400

    tuple and set

---
+++ tuple_and_set.py
@@ -0,0 +1,40 @@
+# Tuple 
+#   -> same as list 
+#   -> can't change the value it's immutable
+#   -> using round brack it implements 
+
+#   Eg 
+tup = (2,3,4,56,3,34,3,4)
+print(tup)
+
+#O/p
+#(2, 3, 4, 56, 3, 34, 3, 4)
+
+# Access index value 
+print('\nSecond index value ',tup[2])
+
+#o/p
+#Second index value  4
+
+#count 
+print('Count of tuple =:',tup.count)
+
+#Advanges 
+# 1 don't change value append insert method not work here
+# 2 Fast excution than list 
+
+# Set 
+#   -> set is collection of unique elements
+#   ->using curly brackets it implements
+
+s = {22,34,32,45,23}
+
+print('\n Set s =>',s)
+
+#O/p
+# Set s => {32, 34, 45, 22, 23}
+
+# set can't give sequence value in python
+# using hash increase the performance 
+# indexing  are not support
+
